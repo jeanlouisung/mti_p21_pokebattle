3499b4942e151f5292c2243fa3a612def33fcc5e
pioupiouz <sebastien.lin@epita.fr>
2020-04-24 19:04:41 +0200

    [BATTLESCREEN] add every sprites

--- app/src/main/java/com/example/mti_p21_pokebattle/fragments/BattleScreen.kt
+++ app/src/main/java/com/example/mti_p21_pokebattle/fragments/BattleScreen.kt
@@ -7,26 +7,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 import com.example.mti_p21_pokebattle.R
-import com.example.mti_p21_pokebattle.adapters.PokedexAdapter
 import com.example.mti_p21_pokebattle.getType
 import com.example.mti_p21_pokebattle.interfaces.PokeapiWebService
-import com.example.mti_p21_pokebattle.models.PokedexPokemonDetail
 import com.example.mti_p21_pokebattle.models.PokemonDetail
 import com.example.mti_p21_pokebattle.models.PokemonStatsSimplified
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.fragment_battle_screen.view.*
-import kotlinx.android.synthetic.main.fragment_pokedex_detail.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.random.Random
 
 /**
  * A simple [Fragment] subclass.
@@ -48,7 +44,8 @@ class BattleScreen : Fragment() {
     private val pokemonsDetail: MutableList<PokemonDetail> = arrayListOf()            // [enemy1, enemy2, enemy3, ally1, ally2, ally3]
     private val pokemonsStats: MutableList<PokemonStatsSimplified> = arrayListOf()   // [enemy1, enemy2, enemy3, ally1, ally2, ally3]
     private var identifiedPokemon : Int = 0
-    private var enemyLeft : Int = 3
+    private var currentEnemyIndex : Int = 0
+    private var currentAllyIndex : Int = 3
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 //        super.onViewCreated(view, savedInstanceState)
@@ -82,7 +79,7 @@ class BattleScreen : Fragment() {
                         // We got our data !
                         val responseData = response.body()
                         if (responseData != null) {
-                            addDataToPokemonsDetail(responseData);
+                            addDataToPokemonsDetail(responseData)
                         }
                     }
                 }
@@ -119,77 +116,103 @@ class BattleScreen : Fragment() {
     private fun sortPokemonsDetail() {
         for (index in 0..5) {
             if (pokemonsDetail[index].id == arguments!!.getInt("enemy1")) {
-                val tmp = pokemonsDetail[0];
-                pokemonsDetail[0] = pokemonsDetail[index];
-                pokemonsDetail[index] = tmp;
+                val tmp = pokemonsDetail[0]
+                pokemonsDetail[0] = pokemonsDetail[index]
+                pokemonsDetail[index] = tmp
                 break;
             }
         }
         for (index in 1..5) {
             if (pokemonsDetail[index].id == arguments!!.getInt("enemy2")) {
-                val tmp = pokemonsDetail[1];
-                pokemonsDetail[1] = pokemonsDetail[index];
-                pokemonsDetail[index] = tmp;
+                val tmp = pokemonsDetail[1]
+                pokemonsDetail[1] = pokemonsDetail[index]
+                pokemonsDetail[index] = tmp
                 break;
             }
         }
         for (index in 2..5) {
             if (pokemonsDetail[index].id == arguments!!.getInt("enemy3")) {
-                val tmp = pokemonsDetail[2];
-                pokemonsDetail[2] = pokemonsDetail[index];
-                pokemonsDetail[index] = tmp;
+                val tmp = pokemonsDetail[2]
+                pokemonsDetail[2] = pokemonsDetail[index]
+                pokemonsDetail[index] = tmp
                 break;
             }
         }
         for (index in 3..5) {
             if (pokemonsDetail[index].id == arguments!!.getInt("ally1")) {
-                val tmp = pokemonsDetail[3];
-                pokemonsDetail[3] = pokemonsDetail[index];
-                pokemonsDetail[index] = tmp;
+                val tmp = pokemonsDetail[3]
+                pokemonsDetail[3] = pokemonsDetail[index]
+                pokemonsDetail[index] = tmp
                 break;
             }
         }
         for (index in 4..5) {
             if (pokemonsDetail[index].id == arguments!!.getInt("ally2")) {
-                val tmp = pokemonsDetail[4];
-                pokemonsDetail[4] = pokemonsDetail[index];
-                pokemonsDetail[index] = tmp;
+                val tmp = pokemonsDetail[4]
+                pokemonsDetail[4] = pokemonsDetail[index]
+                pokemonsDetail[index] = tmp
                 break;
             }
         }
     }
 
     private fun addDataToPokemonsDetail(detail: PokemonDetail) {
-        pokemonsDetail.add(detail);
-        identifiedPokemon++;
+        pokemonsDetail.add(detail)
+        identifiedPokemon++
         if (identifiedPokemon == 6) {
-            sortPokemonsDetail();
-            generatePokemonStats();
-            loadScreen();
+            sortPokemonsDetail()
+            generatePokemonStats()
+            loadScreen()
         }
     }
 
-    private fun newEnemy() {
-        val enemyIndex = 3 - enemyLeft
-        Glide.with(context!!).load(pokemonsDetail[enemyIndex].sprites.front_default)
-            .into(view!!.enemyImage)
-        view!!.enemyName.text = pokemonsDetail[enemyIndex].name
-        view!!.enemyType1.setImageResource(
-            getType(pokemonsDetail[enemyIndex].types[0].type.name)
-        )
-        if (pokemonsDetail[enemyIndex].types.size < 2)
-            view!!.enemyType2.setImageResource(android.R.color.transparent)
-        else
-            view!!.enemyType2.setImageResource(getType(pokemonsDetail[enemyIndex].types[1].type.name))
-        view!!.enemyHPValue.text = pokemonsStats[enemyIndex].hp.toString()
+    private fun updateHP(stats: PokemonStatsSimplified, HPTextView: TextView) {
+        HPTextView.text = stats.hp.toString()
     }
 
-    fun updateEnemy() {
-        val enemyIndex = 3 - enemyLeft
+    private fun loadSprite(detail: PokemonDetail, spriteImageView: ImageView) {
+        Glide.with(context!!).load(detail.sprites.front_default).into(spriteImageView)
+    }
+
+    private fun loadCurrentPokemon() {
+        loadSprite(pokemonsDetail[currentAllyIndex], view!!.currentImage)
+        updateHP(pokemonsStats[currentAllyIndex], view!!.currentHPValue)
+        view!!.currentName.text = pokemonsDetail[currentAllyIndex].name
+        view!!.currentType1.setImageResource(
+            getType(pokemonsDetail[currentAllyIndex].types[0].type.name)
+        )
+        if (pokemonsDetail[currentAllyIndex].types.size < 2)
+            view!!.currentType2.setImageResource(android.R.color.transparent)
+        else
+            view!!.currentType2.setImageResource(getType(pokemonsDetail[currentAllyIndex].types[1].type.name))
+    }
+
+    private fun loadAlliesSprites() {
+        loadSprite(pokemonsDetail[3], view!!.allyImage1)
+        updateHP(pokemonsStats[3], view!!.allyHPValue1)
+        loadSprite(pokemonsDetail[4], view!!.allyImage2)
+        updateHP(pokemonsStats[4], view!!.allyHPValue2)
+        loadSprite(pokemonsDetail[5], view!!.allyImage3)
+        updateHP(pokemonsStats[5], view!!.allyHPValue3)
+        loadCurrentPokemon()
+    }
+
+    private fun newEnemy() {
+        loadSprite(pokemonsDetail[currentEnemyIndex], view!!.enemyImage)
+        updateHP(pokemonsStats[currentEnemyIndex], view!!.enemyHPValue)
+        view!!.enemyName.text = pokemonsDetail[currentEnemyIndex].name
+        view!!.enemyType1.setImageResource(
+            getType(pokemonsDetail[currentEnemyIndex].types[0].type.name)
+        )
+        if (pokemonsDetail[currentEnemyIndex].types.size < 2)
+            view!!.enemyType2.setImageResource(android.R.color.transparent)
+        else
+            view!!.enemyType2.setImageResource(getType(pokemonsDetail[currentEnemyIndex].types[1].type.name))
     }
 
     fun loadScreen() {
-        view!!.enemiesLeftValue.text = enemyLeft.toString()
+        view!!.enemiesLeftValue.text = (3 - currentEnemyIndex).toString()
         newEnemy()
+        loadAlliesSprites()
     }
 }
